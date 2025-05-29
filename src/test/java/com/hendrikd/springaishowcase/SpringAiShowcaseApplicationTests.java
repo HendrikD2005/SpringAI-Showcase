217a049b5b401969653b6e59b99f56d22fbf5956
HendrikD2005 <144935017+HendrikD2005@users.noreply.github.com>
2025-05-29 19:20:51 +0200

    Initial Commit

---
+++ src/test/java/com/hendrikd/springaishowcase/SpringAiShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendrikd.springaishowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiShowcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
