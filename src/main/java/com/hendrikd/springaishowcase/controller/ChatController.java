220842f9b288b287add8d770eb4a7cc90a1ce7b8
HendrikD2005 <144935017+HendrikD2005@users.noreply.github.com>
2025-06-08 14:57:01 +0200

    SpringAI/rag-with-image - Add image description endpoint

--- src/main/java/com/hendrikd/springaishowcase/controller/ChatController.java
+++ src/main/java/com/hendrikd/springaishowcase/controller/ChatController.java
@@ -2,9 +2,17 @@ package com.hendrikd.springaishowcase.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.content.Media;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Controller for chat interactions with the AI model.
@@ -13,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final ChatClient chatClient;
+
+    private List<Media> images;
 
     /**
      * Constructor
@@ -26,23 +36,39 @@ public class ChatController {
         this.chatClient = builder
                 .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
                 .build();
+        this.images = List.of(
+                Media.builder().id("fruits").mimeType(MimeTypeUtils.IMAGE_PNG).data(new ClassPathResource("images/fruits.png")).build(),
+                Media.builder().id("fruits-2").mimeType(MimeTypeUtils.IMAGE_PNG).data(new ClassPathResource("images/fruits-2.png")).build(),
+                Media.builder().id("fruits-3").mimeType(MimeTypeUtils.IMAGE_PNG).data(new ClassPathResource("images/fruits-3.png")).build(),
+                Media.builder().id("fruits-4").mimeType(MimeTypeUtils.IMAGE_PNG).data(new ClassPathResource("images/fruits-4.png")).build(),
+                Media.builder().id("fruits-5").mimeType(MimeTypeUtils.IMAGE_PNG).data(new ClassPathResource("images/fruits-5.png")).build()
+        );
     }
 
     /**
-     * Basic Call
+     * Image Description
      * <p>
-     *     Asks the model what the report says about the top 10 programming languages.
+     *     Asks the model to describe an image from the classpath.
      * </p>
-     *
-     * @return answer from the AI model
+     * @param image Image to describe
+     * @return Image description
      */
-    @GetMapping("/")
-    public String chat() {
-        return chatClient.prompt()
-                .user("What does the report say about the top 10 programming languages?")
+    @GetMapping("/describe/{image}")
+    String describeImage(@PathVariable String image) {
+        Media media = Media.builder()
+                .id(image)
+                .mimeType(MimeTypeUtils.IMAGE_PNG)
+                .data(new ClassPathResource("images/" + image + ".png"))
+                .build();
+
+        UserMessage userMessage = UserMessage.builder().text(
+        """
+        Explain what you see on the image. Explain very detailed.
+        """).media(media).build();
+        Prompt prompt = new Prompt(userMessage);
+
+        return this.chatClient.prompt(prompt)
                 .call()
                 .content();
     }
-
-
 }
